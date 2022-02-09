ff26b6e79438a936a759918ffddedfbf191ea112
Muhammad Taimoor <taimoor5896@gmail.com>
2022-02-09 11:15:10 +0500

    Server Set 3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 8000
 const host= 'localhost'
 var head = '<html><head></head><body><h1> Following are the titles of given websites: </h1><ul>';
 var end = '</ul></body></html>';
-var list='';
+var list= ''
 
 app.listen(port, host, (req,res)=>{
     console.log('listening on port' + port)
@@ -16,25 +16,34 @@ app.get('/I/want/title/', (req, res)=>{
      console.log('Query Params', req.query);
     if(typeof(address) == typeof(''))
    {
-        getTitleAtUrl(address, function(title){
-            list = list +  '<li>' + address +'  -  ' + title + '</li>';
-          });
+        asyncCall(address);
     }
 else
     {
-    
-    var k = 0;    
     for(var i in address)
        { 
-           getTitleAtUrl(address[i], function(title)
-            {
-                list = list +  '<li>' + address[k] +'  -  ' + title + '</li>';
-                k=k+1;
-   
-            },
-            )
+           asyncCall(address[i]);
         };
     }
     res.send(head + list + end) 
 },
 )
+
+
+function getTitle(address) {
+    return new Promise(resolve => {
+        getTitleAtUrl(address, function(title){
+            if(title == undefined || title == null || title == '')
+            {
+                title = 'NO RESPONSE'
+            }
+            list = list +  '<li>' + address +'  -  ' + title + '</li>';
+          });
+          resolve('resolved')
+    });
+  }
+  
+  async function asyncCall(address) {
+    await getTitle(address);
+  }
+    
